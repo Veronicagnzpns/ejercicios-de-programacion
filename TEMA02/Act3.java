f98341e72aae4f6f2e276f1f863c14637d5f4a9a
Verónica González Pons <veronicagnzpns@gmail.com>
2021-10-17 22:11:06 +0200

    act2

---
+++ TEMA02/Act3.java
@@ -0,0 +1,13 @@
+public class Act3 { // Clase principal
+  public static void main(String[] args) {
+    String nombre= "Verónica González Pons";
+    
+    System.out.println("Mi nombre es:" + nombre );
+    String direccion = "c/ Del Romero nº6";
+    
+    System.out.println("Mi direccion es:" + direccion );
+    String telefono = "611470403";
+    
+    System.out.println("Mi telefono es:" + telefono );
+  }
+}
