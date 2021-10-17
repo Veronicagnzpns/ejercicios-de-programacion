26845b4fc06c3ce0495fd3290ac4177def68d207
Verónica González Pons <veronicagnzpns@gmail.com>
2021-10-17 21:56:09 +0200

    act2

---
+++ TEMA02/Act2.java
@@ -0,0 +1,8 @@
+
+public class Act2 { // Clase principal
+  public static void main(String[] args) {
+    String miPalabra = "Verónica González Pons";
+    
+    System.out.println("Mi nombre es:" + miPalabra);
+  }
+}
