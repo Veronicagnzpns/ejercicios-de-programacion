f1600f209e99b5630d86f71d27f91fdf46001691
Verónica González Pons <veronicagnzpns@gmail.com>
2021-10-18 16:07:13 +0200

    act4

---
+++ TEMA02/Act4.java
@@ -0,0 +1,8 @@
+public class Act4 { // Clase principal
+  public static void main(String[] args) {
+    int euros = 100;
+    double pesetas = euros*166.386;
+    System.out.println(euros+" euros son "+pesetas+" pesetas");
+    
+  }
+}
