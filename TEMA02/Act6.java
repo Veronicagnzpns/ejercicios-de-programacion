05a2622ffdc2dc0ff0af509880423fb09dbc0f8e
Verónica González Pons <veronicagnzpns@gmail.com>
2021-10-18 16:37:40 +0200

    act

---
+++ TEMA02/Act6.java
@@ -0,0 +1,5 @@
+public class Act6 { // Clase principal
+  public static void main(String[] args) {
+    double PrecioSinIva = 1500;
+  }
+}
---
+++ TEMA02/Act7.java
@@ -0,0 +1,8 @@
+public class Act7 {
+  public static void main(String[] args){
+    char letra1 = 'v',  letra2 = 'e', letra3 = 'r', letra4 = 'o', letra5 = 'n',letra6 = 'i',letra7 = 'c',letra8 = 'a';
+    String palabra = "gonzalez";
+    System.out.println(""+letra1+letra2+letra3+letra4+letra5+letra6+ letra7 + letra8 + "" + palabra);
+    
+    }
+  }
