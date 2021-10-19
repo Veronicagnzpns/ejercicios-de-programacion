cebaa59979621b4ec4dde0cd5967804ac3ecca36
Verónica González Pons <veronicagnzpns@gmail.com>
2021-10-19 10:15:46 +0200

    Act10

---
+++ TEMA03/Act10.java
@@ -0,0 +1,16 @@
+public class Act10 { // Clase principal
+  public static void main(String[] args) {
+    System.out.print("Por favor, introduce Mb: ");
+    double Mb = Integer.parseInt(System.console().readLine());
+        
+  
+    double total = Mb*1000;
+    
+    
+    
+    
+    System.out.printf(" La conversion a Kb es :  "+ total);
+    
+    
+  }
+}
