ace6b7b78b73d77c87f1f9873b962fd0f0a2995e
Verónica González Pons <veronicagnzpns@gmail.com>
2021-10-24 21:59:54 +0200

    Act5

---
+++ TEMA04/Act5.java
@@ -0,0 +1,16 @@
+public class Act5{
+  public static void main(String[] args) {
+    double a;
+    double b;
+    System.out.print("\nEste programa resuelve ecuaciones de primer grado del tipo ax+b=0 ");
+    System.out.print("\nPorfavor, introduzca el valor de a: ");
+        a = Double.parseDouble(System.console().readLine());
+        System.out.print("ahora introduzca el valor de b: ");
+        b = Double.parseDouble(System.console().readLine());
+    if ( a>0){
+        System.out.println("El resultado de x es " + ((-b )/a));
+}   else {
+  System.out.println("El resultado de la ecuación no tine solución real");
+  }
+}
+}
