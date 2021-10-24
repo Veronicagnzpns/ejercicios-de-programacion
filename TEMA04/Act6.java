a6fc907ec74171e635c71f27a90bdc7742049410
Verónica González Pons <veronicagnzpns@gmail.com>
2021-10-24 22:22:18 +0200

    Act6

---
+++ TEMA04/Act6.java
@@ -0,0 +1,11 @@
+public class Act6{
+  public static void main(String[] args) {
+    double h;
+    double g = 9.81;
+    System.out.print("\nEste programa resuelve ecuaciones de primer grado del tipo ax+b=0 ");
+    System.out.print("\nPorfavor, introduzca el valor de h: ");
+        h = Double.parseDouble(System.console().readLine());
+  System.out.println("El resultado de t es " + (sqrt((2*h)/g));
+  }
+
+}
