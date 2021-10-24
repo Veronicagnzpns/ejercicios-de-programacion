5452d2d3f19cf33958529afd494aa4d3b2e3da4d
Verónica González Pons <veronicagnzpns@gmail.com>
2021-10-24 21:34:07 +0200

    Act4

---
+++ TEMA04/Act4.java
@@ -0,0 +1,21 @@
+public class Act4{
+  public static void main(String[] args) {
+    System.out.print("Por favor, introduce las horas semanales trabajadas: ");
+    double horasTrabajadas= Integer.parseInt(System.console().readLine());
+    
+       
+     if ((horasTrabajadas <=40)) {
+      int precioHora = 12;
+    double salario = horasTrabajadas*precioHora;
+    System.out.println(" El salario de esa semana es: "+ salario+"€");
+    } else if (horasTrabajadas >=41){
+      int precioHora1 = 12;
+      int precioHora2 = 16;
+    double salario = 40*precioHora1 + (horasTrabajadas-40)*precioHora2;
+    System.out.println(" El salario de esa semana es: "+ salario+"€");
+    }
+    
+    
+  }
+}
+
