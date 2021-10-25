c10c7be423f62842364c586c6925b4a7e56cf8bf
Verónica González Pons <veronicagnzpns@gmail.com>
2021-10-25 12:06:23 +0200

    act12

---
+++ TEMA04/Act12.java
@@ -0,0 +1,75 @@
+public class Act12 {
+  public static void main(String[] args) {   
+    System.out.print("1.¿Cuál es la capital de Kiribati? ");
+    System.out.print("a)b)c)");
+    String respuesta = System.console().readLine();
+    int a=0;
+    if (respuesta.equals("a")) {
+       a++;
+    }
+   System.out.print("2.¿Cuál es la capital de Kiribati? ");
+   System.out.print("a)b)c)");
+    String respuesta2 = System.console().readLine();
+   
+    if (respuesta2.equals("b")) {
+       a++;
+    }
+    System.out.print("3.¿Cuál es la capital de Kiribati? ");
+    System.out.print("a)b)c)");
+    String respuesta3 = System.console().readLine();
+    
+    if (respuesta3.equals("b")) {
+       a++;
+    }
+    System.out.print("4.¿Cuál es la capital de Kiribati? ");
+    System.out.print("a)b)c)");
+    String respuesta4 = System.console().readLine();
+    
+    if (respuesta4.equals("c")) {
+       a++;
+    }
+    System.out.print("5.¿Cuál es la capital de Kiribati? ");
+    System.out.print("a)b)c)");
+    String respuesta5 = System.console().readLine();
+   
+    if (respuesta5.equals("a")) {
+       a++;
+    }
+    System.out.print("6.¿Cuál es la capital de Kiribati? ");
+    System.out.print("a)b)c)");
+    String respuesta6 = System.console().readLine();
+    
+    if (respuesta6.equals("c")) {
+       a++;
+    }
+    System.out.print("7.¿Cuál es la capital de Kiribati? ");
+    System.out.print("a)b)c)");
+    String respuesta7 = System.console().readLine();
+    
+    if (respuesta7.equals("a")) {
+       a++;
+    }
+    System.out.print("8.¿Cuál es la capital de Kiribati? ");
+    System.out.print("a)b)c)");
+    String respuesta8 = System.console().readLine();
+    
+    if (respuesta8.equals("b")) {
+       a++;
+    }
+    System.out.print("9.¿Cuál es la capital de Kiribati? ");
+    System.out.print("a)b)c)");
+    String respuesta9 = System.console().readLine();
+   
+    if (respuesta9.equals("c")) {
+       a++;
+    }
+    System.out.print("10.¿Cuál es la capital de Kiribati? ");
+    System.out.print("a)b)c)");
+    String respuesta10 = System.console().readLine();
+   
+    if (respuesta10.equals("c")) {
+       a++;
+    }
+    System.out.print("Tu resultado es: "+ a);
+  }
+}
