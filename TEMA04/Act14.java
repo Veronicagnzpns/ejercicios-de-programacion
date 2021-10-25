b540ddeece8d2b3dec3dbf97027cafbd5ed76b2e
Verónica González Pons <veronicagnzpns@gmail.com>
2021-10-25 12:38:23 +0200

    act14

---
+++ TEMA04/Act14.java
@@ -0,0 +1,13 @@
+public class Act14 {
+  public static void main(String[] args) {   
+     int n1;
+     
+     System.out.print("------PROGRAMA DE NUMEROS------- ");
+    System.out.print("Inserte un numero: ");
+     n1 = Integer.parseInt(System.console().readLine());
+     if(n1){
+       
+       }
+   }
+ }
+   
