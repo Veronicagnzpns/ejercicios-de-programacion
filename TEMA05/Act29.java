51267cd4a4be96a28e42dae955a09820e64faed3
Verónica González Pons <veronicagnzpns@gmail.com>
2021-11-23 10:56:19 +0100

    act

---
+++ TEMA05/Act29.java
@@ -0,0 +1,28 @@
+public class Act29 {
+  public static void main(String[] args) {
+   
+    System.out.print("CALCULO FACTORIAL\n");
+
+    System.out.print("Porfavor introduzca un numero: ");
+    int  n = Integer.parseInt(System.console().readLine());
+    System.out.print("Porfavor introduzca un numero por el que no quieres que se divida: ");
+    int  x = Integer.parseInt(System.console().readLine());
+    
+    int multiplo=1;
+    int total=1;
+
+    
+    
+    for(int i=1; i<n; i++){
+      if ((i%x)!=0){
+        System.out.print( i+" ");
+        
+        }
+    
+      }
+    
+
+
+    }
+    
+}
