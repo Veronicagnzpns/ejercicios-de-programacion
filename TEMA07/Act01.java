4fd89511a50d60d4ba649b989ae209d5aa97256b
Verónica González Pons <veronicagnzpns@gmail.com>
2021-11-30 08:35:17 +0100

    act

---
+++ TEMA07/Act01.java
@@ -0,0 +1,17 @@
+public class Act01 {
+  public static void main(String[] args) {
+        
+    int[] n;
+    n = new int[12]; 
+            
+    n[0] = 39;
+    n[1] = -2;
+    n[4] = 0;
+    n[6] = 14;
+    n[8] = 5;
+    n[9] = 120;
+    System.out.print("Los valores del array n son los siguientes:\n ");
+    System.out.print(n[0] + ", " + n[1] + ", " + n[2] + ", " + n[3]+ ", " + n[4]+ ", " + n[5]+ ", " + n[6]+ ", " + n[7]+ ", " + n[8]+ ", " + n[9]+ ", " + n[10]+ ", " + n[11]);
+    
+  }
+}
