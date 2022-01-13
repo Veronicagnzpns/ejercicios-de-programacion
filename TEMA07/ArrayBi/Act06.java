082bf462edb963435d9ce775fadd3d8466da306e
Verónica González Pons <veronicagnzpns@gmail.com>
2022-01-13 09:53:47 +0100

    act

--- TEMA07/ArrayBi/Act06.java
+++ TEMA07/ArrayBi/Act06.java
@@ -1,8 +1,8 @@
-public class Act05{
+public class Act06{
   public static void main(String[] args) 
   
     throws InterruptedException { 
-       System.out.println("--------------ARRAY ALEATORIO CON POSICION DE MAX Y MIN------------- ");
+       System.out.println("--------------ARRAY ALEATORIO CON POSICION DE MAX Y MIN (sin repetir los numeros)------------- ");
       int[][] n = new int[6][10]; 
       
       int fila;
@@ -13,10 +13,24 @@ public class Act05{
       int nColumnaMax=0;
       int nFilaMin=0;
       int nColumnaMin=0;
+      boolean repetido;
+        int i;
+    int j;
       
       for(fila = 0; fila < 6; fila++) {
       for(columna = 0; columna < 10; columna++) {
-        n[fila][columna] = ((int)(Math.random() * 1001));
+        do {
+          n[fila][columna] = (int)(Math.random() * 1001);
+
+          // Comprueba si el número generado ya está en el array.
+          repetido = false;
+          for (i = 0; i < 10 * fila + columna; i++) {
+            if (n[fila][columna] == n[i / 10][i % 10]) {
+              repetido = true;
+            }
+          }
+        } while (repetido);
+        
         if(n[fila][columna]>max){
           max=n[fila][columna];
           nFilaMax=fila;
@@ -42,5 +56,3 @@ public class Act05{
   }
 
 }
-
-
