99ae5f555b4b842551c98165e20289269afe7954
Veronicagnzpns <veronignzpns@gmail.com>
2022-02-10 08:32:44 +0100

    act

--- TEMA08/MisFunciones/Matematicas.java
+++ TEMA08/MisFunciones/Matematicas.java
@@ -29,12 +29,43 @@ public class Matematicas {
   public static double longitudCircunferencia(double r) {
     return 2 * Math.PI * r;  
   }
+  
   public static int voltea (int n){
 	  int volteado=0;
 	  while(n>0){
       volteado=(volteado*10)+(n%10);
       n/=10;
-    }
+		}
     return volteado;
+	}
+	  
+  public static boolean esCapicua (int n){
+		boolean esCapicua=false;
+		if(voltea(n)==n){
+			esCapicua=true;
+		}
+	return esCapicua;
+		
+     }	
+     
+   public static boolean esPrimo(int n) {
+    boolean esPrimo = true;
+    
+    for (int i = 2; i < n; i++) {
+      if ((n % i) == 0) {
+        esPrimo = false;
+      }
+    }
+    
+    return esPrimo;
+  }	
+  
+  public static int digitos(int n){
+	  int i=0;
+	  for(i=0;n>0;i++){
+		  n/=10;
+		  }
+	   return i;
 	  }
+
 }
--- TEMA08/Prueba1_14.java
+++ TEMA08/Prueba1_14.java
@@ -1,7 +1,10 @@
 import MisFunciones.Matematicas;
 public class Prueba1_14 {
 public static void main(String[] args) {
-	int n=123;
-	System.out.println(Matematicas.voltea(n));
+	int n=12145;
+	/***System.out.println(Matematicas.voltea());**/
+	/**System.out.println(Matematicas.esCapicua(n));**/
+	/**System.out.println(Matematicas.digitos(n));***/
+	
 	}
 }
